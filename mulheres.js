53b7bb1347583ff18d36c15465693383996dbdca
hyngridg <hyngridgradizzi@gmail.com>
2023-09-27 15:17:21 -0300

    Primeira versão da API, implementando os principais verbos HTTP (GET, POST, PATH, DELETE) e salvando os dados em memória do computador.

--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,95 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") //aqui estou iniciando o express
+const router = express.Router() //aqui estou configurando a primeira parte da rota
+const { v4: uuidv4 } = require('uuid')
 
-const app = express()
-const porta = 3333
+const app = express() //aqui estou iniciando o app
+app.use(express.json())
+const porta = 3333 //aqui estou criando a porta
 
+//aqui estou criando a lista inicial de mulheres
 const mulheres = [
     {
+        id: '1',
         nome: 'Hyngrid S',
         imagem: 'https://gitbub.com/hyngridg.png',
         minibio: 'Desenvolvedora'
     },
     {
+        id: '2',
         nome: 'Iana Chan',
         imagem: 'https://bit.ly/3JCXBqP',
         minibio: 'Fundadora da PrograMaria'
     },
     {
+        id: '3',
         nome: 'Luana Pimentel',
         imagem: 'https://bit.ly/3FKpFaz',
         minibio: 'Senior Staff Software Engineer'
     },
 ]
 
+//GET
 function mostraMulheres(request, response){
     response.json(mulheres)
 }
 
+//POST
+function criaMulher(request, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+    }
+    mulheres.push(novaMulher)
+
+    response.json(mulheres)
+}
+
+//PATCH
+function corrigeMulher(request, response) {
+    function encontraMulher(mulher) {
+        if (mulher.id === request.params.id) {
+            return mulher
+        }
+    }
+    const mulherEncontrada = mulheres.find(encontraMulher)
+
+    if (request.body.nome) {
+        mulherEncontrada.nome = request.body.nome
+    }
+
+    if (request.body.minibio) {
+        mulherEncontrada.minibio = request.body.minibio
+    }
+
+    if (request.body.imagem) {
+        mulherEncontrada.imagem = request.body.imagem
+    }
+
+    response.json(mulheres)
+}
+
+//DELETE
+function deletaMulher(request, response) {
+    function todasMenosEla(mulher) {
+        if (mulher.id !== request.params.id) {
+            return mulher
+        }
+    }
+    const mulheresQueFicam = mulheres.filter(todasMenosEla)
+
+    response.json(mulheresQueFicam)
+}
+
+app.use(router.get('/mulheres', mostraMulheres)) //configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher)) //configurei rota POST /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei a rota PATCH /mulheres
+app.use(router.delete('/mulheres/:id', deletaMulher)) //configurei a rota DELETE /mulheres
+
+//PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na ", porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+app.listen(porta, mostraPorta) //servidor ouvindo a porta
